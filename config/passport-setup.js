7dc05c55c96c0feba592147596c6824445c0ea29
Alfonso <anmoya@uc.cl>
2018-10-18 09:12:16 -0300

    GoogleStrategy: reparé el callback

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,13 @@
 const passport          = require('passport');
-const GoogleStrategy    = require('passport-google-oauth20');
+const GoogleStrategy    = require('passport-google-oauth20').Strategy;
 const keys              = require('./keys');
 passport.use(
     new GoogleStrategy({
     // Opciones de la estrategia
+    callbackURL: '/auth/google/redirect',
     clientID : keys.google.clientID,
     clientSecret: keys.google.clientSecret
-    }), () => {
-        // passport callback function
-});
+    }, () => { 
+
+    })
+);
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const passport = require('passport');
+
 
 // Ruta auth/login
 router.get('/login', ( req , res ) => {
-    res.render('login');
+    res.render('login', { user: req.user });
 });
 
 // Logout
@@ -11,8 +13,15 @@ router.get('/logout', ( req, res ) => {
 });
 
 // Autorización Google
-router.get('/google', ( req, res ) => {
-    res.send('logging con gmail');
+router.get('/google', passport.authenticate('google', {
+    // lo que queremos traer desde la información
+    // se pueden usar valores separados por coma [1,2,3]
+    scope: ['profile']
+}));
+
+// callback para redireccionar desde google
+router.get('/google/redirect', ( req, res ) => {
+    res.send('llegaste al callback para google login');
 });
 
 module.exports = router;
